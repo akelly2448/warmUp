536b8df9a96414ef363a8ef79b31051c07c85a73
Melissa Hoo <mhoo@dev-10.com>
2021-07-27 09:59:31 -0500

    back to before

--- git-practice/src/main/java/App.java
+++ git-practice/src/main/java/App.java
@@ -2,11 +2,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("testing testing 1 2 3");
-<<<<<<< HEAD
-
-=======
-        System.out.println("beep boop bop");
->>>>>>> 7f34d0f890ab5a3927b9a3cdffea501d062b08cb
+        System.out.println("hello");
     }
 
 }

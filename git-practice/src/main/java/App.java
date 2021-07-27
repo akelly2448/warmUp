3322718167a22e500fb5538b7d087e2267b30531
unknown <akelly2448@gmail.com>
2021-07-27 10:49:26 -0400

    trying to commit change

--- git-practice/src/main/java/App.java
+++ git-practice/src/main/java/App.java
@@ -2,6 +2,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("testing testing 1 2 3");
+        System.out.println("is this how you do the merge conflict?");
     }
 
 }
